343b9c9e9ae94920bcbdf4f12d191a92e439cb9c
Isabelle Motte <38882780+zabellemotte@users.noreply.github.com>
2024-09-03 10:45:26 +0200

    Create block_socialflow.php

---
+++ lang/en/block_socialflow.php
@@ -0,0 +1,69 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_news_items', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   block_socialflow
+ * @copyright 2024 Zabelle Motte (UCLouvain)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['socialflow:addinstance'] = 'Add a "Social flow" block';
+$string['pluginname'] = 'Social flow';
+$string['privacy:metadata'] = 'The Social Flow A CONFIGURER.';
+$string['blocktitle'] = 'Social flow';
+$string['helpbuttontext']="Help";
+$string['helptext']="<p>The social feed exploits the traces of student activities in the platform to aggregate them in an anonymised way and offer a view of the list of resources and activities that most engage the attention of students. This tool has been developed to support students' learning and help them set goals for themselves on a daily basis.</p>
+<p>The tool's customisation options are as follows: <ul>
+<li>  choose the reference period from 1 day to 2 weeks, to be personalised according to your relationship to deadlines,  </li>
+<li> operate a selection of courses taken, to be personalised according to your expectations of each course, </li>
+<li> display actions related to consultations (such as watching a video), contributions (such as submitting an assignment), or both, </li>
+<li> choose the number of lines displayed in the block (from 5 to 100). </li>
+</ul></p>
+<p>
+Some resources or activities appear grayed out in the list because they are configured with a restriction and you can not access them. Therefore, you should not take these items into account. </p>";
+$string['surveytextintro']="<div>Do not forget to  ";
+$string['surveytextlink']="give us your opinion on social feed";
+$string['surveylink']="https://www.uclouvain.be";
+$string['surveytextend']=".</div>";
+$string['option1'] = '2 weeks';
+$string['option2'] = '1 week';
+$string['option3'] = '3 days';
+$string['option4'] = '1 day';
+$string['value1'] = '14';
+$string['value2'] = '7';
+$string['value3'] = '3';
+$string['value4'] = '1';
+$string['osotext'] = 'Reference period'; // oso=option selection option
+$string['csotext'] = 'Courses'; // cso=course selection option
+$string['type1']='Consultation';
+$string['type2']='Contribution';
+$string['type3']='Both';
+$string['consult']='Consultation';
+$string['contrib']='Contribution';
+$string['tsotext']='Actions'; //tso=type selection option
+$string['insotext']='Number of lines'; //inso=item number selection option
+$string['save'] = 'Save'; 
+$string['close'] = 'Close'; 
+$string['nodata'] = 'No data available, come back later or modify selection options'; 
+$string['done'] = 'Done';
+$string['todo'] = 'To do';
+$string['restricted'] = 'No access';
+$string['nextupdate'] = 'Next update : ';
+$string['limitdate'] = "Deadline : ";
+$string['latedate'] = 'Late ! <br> Closing : ';
+$string['closed'] = 'Closed ! ';
