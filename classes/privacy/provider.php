0fb0c06099aa42e793da02fe42177ff43e36ab3b
Isabelle Motte <38882780+zabellemotte@users.noreply.github.com>
2025-12-19 14:07:40 +0100

    Update provider.php
    php coding error correction

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -29,7 +29,7 @@ namespace block_socialflow\privacy;
  *
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class provider implements \core_privacy\local\metadata\null_provider {
+class provider implements \core_privacy\local\metadata\null_provider{
 
     /**
      * Get the language string identifier with the component's language
